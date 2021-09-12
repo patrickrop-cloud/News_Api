c783114583452db0343292f19b738241ea8eea0d
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-09-12 21:05:58 +0300

    updated init filr on app folder for configurations and bootstrapping feature.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,27 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap, bootstrap_find_resource
 from config import config_options
 
-#Initializing application
-app = Flask(__name__)
 
-from app import views
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    #creating app configurations
+    app.config.from_object(config_options[config_name])
+
+    #Initializing flask extensions
+    bootstrap.init_app(app)
+
+    #Registering blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    #Setting config
+    from .requests import configure_request
+    configure_request(app)
+
+    #will add views and forms
+    return app
+
