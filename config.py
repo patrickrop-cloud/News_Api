e79dd8e95aed6d30f9d2eeaac8765e905ebd623c
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-09-12 22:05:58 +0300

    edited config file.

--- config.py
+++ config.py
@@ -2,9 +2,10 @@ import os
 
 class Config:
 
-    NEWS_API_BASE_URL ='https://newsapi.org/v2/everything?q=apple&from=2021-09-10&to=2021-09-10&sortBy=popularity&apiKey={}'
+    NEWS_API_BASE_URL ='https://newsapi.org/v2/everything?q=apple&from=2021-09-10&to=2021-09-10&sortBy=popularity&apiKey={}'  
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    NEWS_ARTICLE_URL ='https://newsapi.org/v2/everything?sources={}&apiKey={}'
+    # SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 class ProdConfig(Config):
