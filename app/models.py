1071bbb311f36056e0354fae67f995a670c6c5bc
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-09-12 23:15:36 +0300

    Updated models.py file to create classes for our news and articles.

---
+++ app/models.py
@@ -0,0 +1,28 @@
+class News:
+    '''
+    News class to define News Objects
+    '''
+
+    def __init__(self,id,name,category,country,url,description):
+        self.id = id
+        self.name = name
+        self.category = category
+        self.country = country
+        self.url = url
+        self.description = description
+
+
+class NewsArticle:
+
+    def __init__(self,id,title,author,description,url,urlToImage,publishedAt,content):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
+    
+    # def save_review(self):
+    #     Review.all_reviews.appe
