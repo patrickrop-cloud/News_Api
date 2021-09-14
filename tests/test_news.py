54d550aebaab207868fc4e58df642f4258f4490e
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-09-14 10:05:05 +0300

    add test

---
+++ tests/test_news.py
@@ -0,0 +1,19 @@
+import unittest
+from app.models import News
+
+class NewsTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the News class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_news = News('Thriling api news. Watchout!')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_news,News))
+
+    if __name__ == '__main__':
+        unittest.main()
